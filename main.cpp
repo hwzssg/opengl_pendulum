c8a36bba453ee4111b20c59b8d1b8f9f1606a1bb
iamareebjamal <jamal.areeb@gmail.com>
2017-03-01 17:46:19 +0530

    feat: Load initial seconds into the clock

--- main.cpp
+++ main.cpp
@@ -2,14 +2,15 @@
 #include "draw.h"
 #include <GL/glut.h>
 #include <cmath>
+#include <ctime>
 
 using namespace std;
 
 
 // Pendulum related variables
 double angle = M_PI / 6;
-double angleAccel, angleVelocity = 0, dt = 0.1;
-float length = 9.55;
+double angleAcceleration, angleVelocity = 0, dt = 0.1;
+float length = 9.44;
 
 // Clock related variables
 int clocks, seconds;
@@ -70,24 +71,26 @@ void drawFunc() {
 
     glMatrixMode(GL_MODELVIEW);
 
-    angleAccel = -9.81 / length * sin(angle);
-    angleVelocity += angleAccel * dt;
+    angleAcceleration = -9.81 / length * sin(angle);
+    angleVelocity += angleAcceleration * dt;
     angle += angleVelocity * dt;
 
     drawBob(angle);
     drawClock();
 
     clocks++;
-    if(clocks > 60) {
+    if(clocks%60 == 0)
         seconds++;
-        clocks = 0;
-    }
-
 
     glutSwapBuffers();
 }
 
 int main(int argc, char **argv) {
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+    seconds = ltm->tm_sec + 60*ltm->tm_min + 60*60*ltm->tm_hour;
+
+
     panel = new draw(argc, argv);
     panel->setTitle("Pendulum Simulation");
     panel->setDrawFunction(drawFunc);
